53b190802d10a30f707c274b7fcf5588c92b8aac
Esteban Dulaurans <esteban.dulaurans@epita.fr>
2023-11-17 00:33:19 +0100

    feat: base configuration

---
+++ lua/nyxhund/init.lua
@@ -0,0 +1,36 @@
+require("nyxhund.remap")
+
+vim.opt.swapfile = false
+vim.cmd.colorscheme "catppuccin-macchiato"
+
+vim.opt.tabstop = 8
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+vim.opt.ignorecase = false
+vim.opt.smartcase = true
+
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.colorcolumn = "80"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.signcolumn = "yes"
+vim.opt.termguicolors = true
+vim.opt.showmode = false
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.wildmenu = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.autoread = true
+vim.opt.autowrite = true
+vim.opt.hidden = true
+vim.opt.swapfile = false
+
+vim.opt.mouse = a
+vim.opt.list = true
+vim.opt.listchars = { trail = "·" }
