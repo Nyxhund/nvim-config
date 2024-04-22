13d32ece4d2888c2b71d745e0fc1c8e16eae52f1
Esteban Dulaurans <esteban.dulaurans@epita.fr>
2024-04-23 01:04:09 +0200

    feat: Base Greeter, to add commands

---
+++ lua/plugins/greeter.lua
@@ -0,0 +1,35 @@
+return {
+    'goolord/alpha-nvim',
+    config = function ()
+        local alpha = require'alpha'
+        local dashboard = require'alpha.themes.dashboard'
+        dashboard.section.header.val = {
+            [[            (  )   (   )  )                      ]],
+            [[             ) (   )  (  (                       ]],
+            [[             ( )  (    ) )                       ]],
+            [[             _____________                       ]],
+            [[            <_____________> ___                  ]],
+            [[            |             |/ _ \                 ]],
+            [[            |               | | |                 ]],
+            [[            |               |_| |                ]],
+            [[         ___|             |\___/                 ]],
+            [[        /    \___________/    \                  ]],
+            [[        \_____________________/                  ]],
+            [[                                                 ]],
+        }
+        dashboard.section.buttons.val = {
+            dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
+            dashboard.button( "q", "󰅚  Quit NVIM" , ":qa<CR>"),
+        }
+        local handle = io.popen('fortune')
+        local fortune = handle:read("*a")
+        handle:close()
+        dashboard.section.footer.val = fortune
+
+        dashboard.config.opts.noautocmd = true
+
+        vim.cmd[[autocmd User AlphaReady echo 'Ready']]
+
+        alpha.setup(dashboard.config)
+    end
+};
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     config = {
-         ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'terraform', 'yaml', 'html', 'tsx', 'js' }
+        ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'terraform', 'yaml', 'html', 'tsx', 'js' }
     }
 }
